d945d001506292193e317858bae5f38b9b902eb7
meriadec <meriadec.pillet@gmail.com>
2017-02-09 23:28:30 +0100

    refactor test folder

--- test/output-tests.js
+++ test/output-tests.js
@@ -4,24 +4,25 @@ import fs from 'fs'
 
 import convertSVG from '../src/convertSVG'
 
-const SVG_FOLDER = path.resolve(__dirname, 'fixtures/svgs')
-const EXPECTED_FOLDER = path.resolve(__dirname, 'fixtures/expected')
+const FIXTURES_FOLDER = path.resolve(__dirname, 'fixtures')
 
 describe('output with default options', () => {
 
-  const inputFiles = fs.readdirSync(SVG_FOLDER)
+  const inputFiles = fs.readdirSync(FIXTURES_FOLDER)
 
   inputFiles.forEach(file => {
 
+    if (file.endsWith('.js')) { return }
+
     it(`testing output of ${file}`, async () => {
       const content = fs.readFileSync(
-        path.resolve(SVG_FOLDER, file),
+        path.resolve(FIXTURES_FOLDER, file),
         { encoding: 'utf8' }
       )
       const actual = await convertSVG(content)
       const expectedFile = `${file.substr(0, file.indexOf('.'))}.js`
       const expected = fs.readFileSync(
-        path.resolve(EXPECTED_FOLDER, expectedFile),
+        path.resolve(FIXTURES_FOLDER, expectedFile),
         { encoding: 'utf8' }
       )
       expect(actual).toEqual(expected)
